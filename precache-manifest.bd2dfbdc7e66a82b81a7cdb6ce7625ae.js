43c9d0b85b2c8273ef9e50be83b1e1de97cdc81b
ralucabaksai <62606434+ralucabaksai@users.noreply.github.com>
2020-08-04 17:03:12 +0300

    Updates

---
+++ precache-manifest.bd2dfbdc7e66a82b81a7cdb6ce7625ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33575147099ad0f30d49fab459a9984b",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "32d21be2fdc58ed0938e",
+    "url": "/blog/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c13e09d8d057b17fb2f8",
+    "url": "/blog/static/css/main.e2059eeb.chunk.css"
+  },
+  {
+    "revision": "32d21be2fdc58ed0938e",
+    "url": "/blog/static/js/2.6a274c2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blog/static/js/2.6a274c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13e09d8d057b17fb2f8",
+    "url": "/blog/static/js/main.d04b91e4.chunk.js"
+  },
+  {
+    "revision": "530da1a68349993fc94b",
+    "url": "/blog/static/js/runtime-main.c5e0939b.js"
+  }
+]);
