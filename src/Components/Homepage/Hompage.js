6ae25da95c06c50c01a66d4c2e67092a1e922d9d
ralucabaksai <62606434+ralucabaksai@users.noreply.github.com>
2020-08-04 15:50:07 +0300

    v2

--- src/Components/Homepage/Hompage.js
+++ src/Components/Homepage/Hompage.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div className='homepage'>
+      <h1>Hy i'm Raluca</h1>
+      <p>Check out my blog!</p>
+    </div>
+  );
 }
 
 export default Homepage;
