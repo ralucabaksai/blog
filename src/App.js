b0dc2bc95d11f22d1f2c6d238e1e9c8c2375f903
ralucabaksai <62606434+ralucabaksai@users.noreply.github.com>
2020-08-04 17:05:56 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import Blogpage from "./Components/Blogpage/Blogpage";
 function Main() {
   return (
     <Switch>
-      <Route exact path='/' component={Homepage} />
-      <Route exact path='/blogpage' component={Blogpage} />
+      <Route exact path='/blog/' component={Homepage} />
+      <Route exact path='/blog//blogpage' component={Blogpage} />
     </Switch>
   );
 }
